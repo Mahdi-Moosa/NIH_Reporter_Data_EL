622248ae845ab2abfb7fe82890f687a6cf101726
Mahdi Moosa <mahdi.moosa@gmail.com>
2023-02-25 03:38:14 +0000

    file download function

--- nih_reporter_etl.py
+++ nih_reporter_etl.py
@@ -1,3 +1,14 @@
 import pandas as pd
+from prefect import flow, task
+import os
 
-print(pd.__version__)
+# @task(log_prints=True, retries=3)
+def download_file(data_year: int, data_type: str = "projects", save_dir_pref: str= 'data') -> None:
+    save_dir = f'{save_dir_pref}/{data_type}'
+    url = f"https://reporter.nih.gov/exporter/{data_type}/download/{data_year}"
+    file_path = f"{save_dir}/{data_year}.zip"
+    if not os.path.isdir(save_dir):
+        os.makedirs(save_dir)
+    os.system(f'wget -O {file_path} {url}')
+
+download_file(data_year= 2020)
